495b5bbc9cb2c6edbb76b5715f6300a9072f920d
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-10-20 22:11:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello, pancakes.")
+class Pancake:
+  def __init__(self):
+    self.stacks = []
+
+  def append(self, stack):
+    self.stacks.append(stack)
+
+class InputSelect:
+  def __init__(self, options):
+    self.options = options
+
+  def input(self):
+    for option in options:
+      print(f"[0] {option}"
+
+print("Welcome to 'Pancake Simulator 2022'.")
+print("What would you like to do?")
